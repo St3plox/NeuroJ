bb68e73bbd1fe266314e87a4287ebf17d1aa746b
st3pegor <st3pegor@gmail.com>
2023-10-22 17:33:16 +0700

    Connection

--- src/main/java/com/tveu/neuroj/core/Connection.java
+++ src/main/java/com/tveu/neuroj/core/Connection.java
@@ -1,4 +1,43 @@
 package com.tveu.neuroj.core;
 
 public class Connection {
+
+    private final Neuron fromNeuron;
+
+    private final Neuron toNeuron;
+
+    private Weight weight;
+
+    public Connection(Neuron fromNeuron, Neuron toNeuron) {
+        this.fromNeuron = fromNeuron;
+        this.toNeuron = toNeuron;
+
+        weight = new Weight();
+    }
+
+    public Connection(Neuron fromNeuron, Neuron toNeuron, Weight weight) {
+        this.fromNeuron = fromNeuron;
+        this.toNeuron = toNeuron;
+        this.weight = weight;
+    }
+
+    public final Neuron getFromNeuron() {
+        return fromNeuron;
+    }
+
+    public final Neuron getToNeuron() {
+        return toNeuron;
+    }
+
+    public final Weight getWeight() {
+        return weight;
+    }
+
+    public final void setWeight(Weight weight) {
+        this.weight = weight;
+    }
+
+    public double getWeightedInput() {
+        return fromNeuron.getOutput() * weight.getValue();
+    }
 }
