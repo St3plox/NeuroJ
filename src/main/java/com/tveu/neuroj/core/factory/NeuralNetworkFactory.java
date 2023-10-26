38b16555c1db2c7134d315fa98bb01040d0fcad7
st3pegor <st3pegor@gmail.com>
2023-10-26 22:52:29 +0700

    added NeuralNetworkFactory

---
+++ src/main/java/com/tveu/neuroj/core/factory/NeuralNetworkFactory.java
@@ -0,0 +1,52 @@
+package com.tveu.neuroj.core.factory;
+
+import com.tveu.neuroj.core.Layer;
+import com.tveu.neuroj.core.NeuralNetwork;
+import com.tveu.neuroj.core.function.AbstractActivationFunction;
+import com.tveu.neuroj.core.function.SigmoidFunction;
+import com.tveu.neuroj.core.neuron.InputNeuron;
+
+public class NeuralNetworkFactory {
+
+    public final static String INPUT_LAYER_LABEL = "INPUT";
+    public final static String HIDDEN_LAYER_LABEL = "HIDDEN";
+    public final static String OUTPUT_LAYER_LABEL = "OUTPUT";
+
+    private final NeuralNetwork neuralNetwork;
+
+    public NeuralNetworkFactory() {
+        this.neuralNetwork = new NeuralNetwork();
+    }
+
+    public NeuralNetworkFactory addInputLayer(int numberOfNeurons) {
+        return addLayer(INPUT_LAYER_LABEL, numberOfNeurons, new SigmoidFunction());
+    }
+
+    public NeuralNetworkFactory addHiddenLayer(int numberOfNeurons, AbstractActivationFunction activationFunction) {
+        return addLayer(HIDDEN_LAYER_LABEL, numberOfNeurons, activationFunction);
+    }
+
+    public NeuralNetworkFactory addOutputLayer(int numberOfNeurons, AbstractActivationFunction activationFunction) {
+        return addLayer(OUTPUT_LAYER_LABEL, numberOfNeurons, activationFunction);
+    }
+
+    private NeuralNetworkFactory addLayer(String label, int numberOfNeurons, AbstractActivationFunction activationFunction) {
+        Layer layer = new Layer(numberOfNeurons);
+        layer.setLabel(label);
+        neuralNetwork.addLayer(layer);
+        return this;
+    }
+
+    public NeuralNetwork build() {
+        return neuralNetwork;
+    }
+
+    public static NeuralNetworkFactory createDefaultNeuralNetwork() {
+        return new NeuralNetworkFactory()
+                .addInputLayer(3)
+                .addHiddenLayer(5, new SigmoidFunction())
+                .addHiddenLayer(5, new SigmoidFunction())
+                .addOutputLayer(1, new SigmoidFunction());
+    }
+
+}
