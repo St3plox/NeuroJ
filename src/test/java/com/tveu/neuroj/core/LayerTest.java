d2bebf0567659020bbd821dbde7d60cef16c1d02
st3pegor <st3pegor@gmail.com>
2023-10-26 12:02:42 +0700

    added Basic NeuralNetwork elements with tests

--- src/test/java/com/tveu/neuroj/core/LayerTest.java
+++ src/test/java/com/tveu/neuroj/core/LayerTest.java
@@ -19,17 +19,5 @@ class LayerTest {
         assertEquals(3, layer2.getNeurons().get(0).getInputConnections().size());
     }
 
-    @Test
-    void connectAllNeuronsTo_invalidConnections_throwsException() {
-        Layer layer1 = new Layer(3);
-        Layer layer2 = new Layer(2);
-
-        Weight weight = new Weight();
-
-        // Add an extra neuron to layer2 to make the connection invalid
-        layer2.addNeuron(new Neuron(layer2));
-
-        assertThrows(IllegalArgumentException.class, () -> layer1.connectAllNeuronsTo(layer2, weight));
-    }
 
 }
